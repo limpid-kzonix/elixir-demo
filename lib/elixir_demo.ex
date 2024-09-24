2241aaf3e8851e2a0aae12a398fc6235570a986a
limpid-kzonix <balyszyn@gmail.com>
2024-09-24 19:44:01 +0300

    Initial commit

---
+++ lib/elixir_demo.ex
@@ -0,0 +1,18 @@
+defmodule ElixirDemo do
+  @moduledoc """
+  Documentation for `Example`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Example.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
